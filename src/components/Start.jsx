20fcc24f84a98592e1242506dc2eb49f63404351
arashkhattal <arashkhattal@gmail.com>
2022-10-02 23:42:10 +0530

    initail commit

---
+++ src/components/Start.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { startQuiz } from "../redux/action/quizAction";
+import welcome from "../audio/welcome.mp3"
+const Start = () => {
+  const dispatch = useDispatch();
+  // const {time} = useSelector(state => state.quizReducer)
+  const [minute, setMinute] = useState(1);
+  const [second, setSecond] = useState(0);
+  const [time, setTime] = useState(60);
+  const handleQuizStart = () => {
+    document.getElementById('welcome').play();
+    dispatch(startQuiz(time));
+  };
+  useEffect(() => {
+    if (minute > 59) {
+      setMinute(1);
+    }
+    if (second > 59) {
+      setSecond(1);
+    }
+  }, [minute, second]);
+  useEffect(() => {
+    console.log(minute);
+    console.log(typeof minute);
+    if (minute !== NaN && second !== NaN) {
+      document.getElementById('welcome').play();
+      setTime(minute * 60 + second);
+    }
+  }, [minute, second]);
+  return (
+    <div className="startBox">
+    <div>
+      <audio id="welcome" src={welcome}></audio>
+    </div>
+      <div>
+        <div>
+          <h1>Start the Quiz</h1>
+          <p>Good luck!</p>
+          <p>Time:&nbsp;&nbsp;{time}sec</p>
+          <section>
+            <label htmlFor="">
+              <input
+                type="number"
+                className="timeInput"
+                value={minute}
+                onChange={(e) => setMinute(parseInt(e.target.value))}
+              />
+              min
+            </label>
+            <label htmlFor="">
+              <input
+                type="number"
+                className="timeInput"
+                value={second}
+                onChange={(e) => setSecond(parseInt(e.target.value))}
+              />
+              sec
+            </label>
+          </section>
+          <button className="startButton" onClick={handleQuizStart} data-aos="fade-up">
+            START
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Start;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// redux provider
+import { Provider } from "react-redux";
+import { store } from "./redux/reducer/store";
+// redux persist
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
+let persistor = persistStore(store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
